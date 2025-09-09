199b1878ccdeaef9e5f029a4544f799ca708c1d5
Shiva Pal <shivapal108941@gmail.com>
2025-09-09 14:04:46 +0530

    Add : KONG gateway route

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,24 +1,36 @@
-import { CreateUserData, Credentials } from "../types";
+import { CreateUserData, Credentials, Tenant } from "../types";
 import { api } from "./client";
+
+// CONSTANTS : FOR KONG GATEWAY
+export const AUTH_SERVICE = `/kong/api/auth`;
+export const CATALOG_SERVICE = `/kong/api/catalog`;
 
 //Auth Service
 export const login = (credentials: Credentials) =>
-  api.post("/auth/login", credentials);
-export const self = () => api.get("/auth/self");
-export const logout = () => api.post("/auth/logout");
+  api.post(`${AUTH_SERVICE}/auth/login`, credentials);
+export const self = () => api.get(`${AUTH_SERVICE}/auth/self`);
+export const logout = () => api.post(`${AUTH_SERVICE}/auth/logout`);
 
-// User Service
+// Auth Service
+// a. User Service
 export const getUsers = (queryString: string) =>
-  api.get("/users?" + queryString);
-export const createUser = (user: CreateUserData) => api.post("/users", user);
+  api.get(`${AUTH_SERVICE}/users?` + queryString);
+export const createUser = (user: CreateUserData) =>
+  api.post(`${AUTH_SERVICE}/users`, user);
 export const updateUser = (id: string, user: CreateUserData) =>
-  api.patch(`/users/${id}`, user);
-export const deleteUser = (id: string) => api.delete(`/users/${id}`);
+  api.patch(`${AUTH_SERVICE}/users/${id}`, user);
+export const deleteUser = (id: string) =>
+  api.delete(`${AUTH_SERVICE}/users/${id}`);
 
-// Tenant Service
+// b. Tenant Service
 export const getTenants = (queryString: string) =>
-  api.get("/tenants?" + queryString);
-export const createTenant = (tenant: any) => api.post("/tenants", tenant);
-export const updateTenant = (id: string, tenant: any) =>
-  api.patch(`/tenants/${id}`, tenant);
-export const deleteTenant = (id: string) => api.delete(`/tenants/${id}`);
+  api.get(`${AUTH_SERVICE}/tenants?` + queryString);
+export const createTenant = (tenant: Tenant) =>
+  api.post(`${AUTH_SERVICE}/tenants`, tenant);
+export const updateTenant = (id: string, tenant: Tenant) =>
+  api.patch(`${AUTH_SERVICE}/tenants/${id}`, tenant);
+export const deleteTenant = (id: string) =>
+  api.delete(`${AUTH_SERVICE}/tenants/${id}`);
+
+// Catalog Service
+export const getCategories = () => api.get(`${CATALOG_SERVICE}/categories`);
